0edb33f13e010c5778fb31aad30dea0a3e3efb02
Ricardo Escobar <rescobar713@bitbucket.org>
2016-09-10 14:38:08 -0500

    Added InputTest script

---
+++ Assets/Scripts/InputTest.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputTest : MonoBehaviour {
+
+    float xDir;
+    float yDir;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        xDir = Input.GetAxis("Horizontal");
+        yDir = Input.GetAxis("Vertical");
+
+        Vector2 unitDir = new Vector2(xDir, yDir).normalized;
+
+        Debug.Log(unitDir);
+	}
+}
