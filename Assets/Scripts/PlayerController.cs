acaebc3f329f3855d029df147950926c3564d04e
Erin Pennington <epennington@utexas.edu>
2016-09-10 13:56:23 -0500

    test scene

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		player = gameObject;
+	}
+
+	// FixedUpdate called once per physics tick
+	void FixedUpdate() {
+		// Movement
+		var x = Input.GetAxis("Horizontal") * Time.deltaTime;
+		var z = Input.GetAxis("Vertical") * Time.deltaTime;
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
