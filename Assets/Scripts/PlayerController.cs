84e55dee3309ce6caf743fb02280664faaccc1a4
Erin Pennington <epennington@utexas.edu>
2016-09-10 17:03:25 -0500

    made a parent class of playercontroller which is cactuscontroller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,35 +1,9 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerController : MonoBehaviour {
-
-	public GameObject player;
-	public Rigidbody2D rbody;
+public class PlayerController : CactusController {
 
 	public float speed = 0f;
-
-	bool spikesExtended = false;
-	bool spikesRetracting = false;
-
-    public int numSpikes;
-
-    public GameObject spikePrefab;
-
-    public float spikeExpandSpeed;
-
-	// Use this for initialization
-	void Start () {
-		player = gameObject;
-		rbody = GetComponent<Rigidbody2D>();
-
-        float spikeSpacing = 360f / numSpikes;
-        for(int i = 0; i < numSpikes; i++)
-        {
-            GameObject newSpike = GameObject.Instantiate(spikePrefab);
-            newSpike.transform.rotation = Quaternion.Euler(new Vector3(0, 0, i * spikeSpacing));
-            newSpike.transform.parent = transform;
-        }
-	}
 
 	// Update is called once per frame
 	void Update () {
@@ -46,39 +20,6 @@ public class PlayerController : MonoBehaviour {
 		if (Input.GetKeyDown ("space")) {
 			if (!spikesExtended)
 				ExtendSpikes ();
-		}
-	}
-
-	void ExtendSpikes () {
-		spikesExtended = true;
-		foreach (Transform child in transform) {
-			var sprite = child.GetChild (0);
-			// enable spike collisions while extended
-			sprite.GetComponent<Collider2D> ().enabled = true;
-			iTween.MoveBy (sprite.gameObject, iTween.Hash("x", 0f, "y", 0.5f, "z", 0f, "oncomplete", "RetractSpikes", "oncompletetarget", gameObject, "time", 1));
-		}
-	}
-
-	void RetractSpikes () {
-		if (!spikesRetracting) {
-			spikesRetracting = true;
-			foreach (Transform child in transform) {
-				var sprite = child.GetChild (0);
-				iTween.MoveBy (sprite.gameObject, iTween.Hash ("x", 0f, "y", -0.5f, "z", 0f, "oncomplete", "OnRetractSpikesComplete", "oncompletetarget", gameObject, "time", 1));
-			}
-		}
-	}
-
-	void OnRetractSpikesComplete() {
-		if (spikesRetracting) {
-			spikesExtended = false;
-			spikesRetracting = false;
-
-			// disable spike collisions while retracted
-			foreach (Transform child in transform) {
-				var sprite = child.GetChild (0);
-				sprite.GetComponent<Collider2D> ().enabled = false;
-			}
 		}
 	}
 }
