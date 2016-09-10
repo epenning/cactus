cdc9d29864154f1b9e32664e171b3d462fb386a3
Erin Pennington <epennington@utexas.edu>
2016-09-10 16:11:48 -0500

    starting to implement spike collisions

---
+++ Assets/Scripts/SpikeController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpikeController : MonoBehaviour {
+
+	public GameObject parentGamObject;
+
+	// Use this for initialization
+	void Start () {
+		parentGamObject = transform.parent.parent.gameObject;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter2D(Collision2D coll) {
+		// not our own gameobject
+		if (coll.gameObject.tag != parentGamObject.tag) {
+			Debug.Log ("Hit something!");
+		}
+	}
+}
