04f0e2f39d28a27672f2f688c27004137c0d1c1a
Ethan <ethanle98@gmail.com>
2016-09-11 01:05:04 -0500

    Made background into a tileable texture - see desc
    Desert ground was turned into a texture, along with the addition of an
    unlit material. The material can be attached to a object with a quadmesh
    and further scripting would allow for a scalable background by tiling
    the texture to automatically repeat. Also ignor BackgroundTiler.cs, I
    didn't know how to program the math calculations for said scaling.

---
+++ Assets/Scripts/BackgroundTiler.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundTiler : MonoBehaviour {
+    // Use this for initialization
+    public int textureUnitSize;
+    public int gameWidth;
+    public int gameHeight;
+	void Start () {
+        gameObject.transform.localScale.Set(gameWidth/textureUnitSize, gameHeight/textureUnitSize, 1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
